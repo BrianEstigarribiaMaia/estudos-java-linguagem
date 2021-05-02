aa65c91ed2e5dbfeecba1f8fc90cfa17357d42d3
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-05-02 14:37:43 -0300

    Add files via upload

---
+++ DiasDaSemana.java
@@ -0,0 +1,5 @@
+package br.com.estudos.java;
+
+public enum DiasDaSemana {
+	segunda, terça, quarta, quinta, sexta, sábado, domingo
+}
---
+++ ExecutaDiasDaSemana.java
@@ -0,0 +1,13 @@
+package br.com.estudos.java;
+
+public class ExecutaDiasDaSemana {
+
+	public static void main(String[] args) {
+		
+		for(DiasDaSemana dias: DiasDaSemana.values()){
+			System.out.println(dias);
+		}
+
+	}
+
+}
