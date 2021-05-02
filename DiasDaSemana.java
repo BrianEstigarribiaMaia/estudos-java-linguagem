7cd5c164b80a0bd460731f81bf20ec58c235fd5a
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-05-02 14:38:26 -0300

    Update and rename DiasDaSemana.java to enum/DiasDaSemana.java

--- DiasDaSemana.java
+++
@@ -1,5 +0,0 @@
-package br.com.estudos.java;
-
-public enum DiasDaSemana {
-	segunda, terça, quarta, quinta, sexta, sábado, domingo
-}
---
+++ enum/DiasDaSemana.java
@@ -0,0 +1,5 @@
+package br.com.estudos.java.enums;
+
+public enum DiasDaSemana {
+	segunda, terça, quarta, quinta, sexta, sábado, domingo
+}
