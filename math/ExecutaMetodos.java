1649e69e42e08f01e9d4b17b02336dd1971d8f13
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-05-07 17:28:53 -0300

    Rename ExecutaMetodos.java to math/ExecutaMetodos.java

--- math/ExecutaMetodos.java
+++ math/ExecutaMetodos.java
@@ -7,9 +7,9 @@ public class ExecutaMetodos {
 		double numero2 = 5;
 		double numero3 = 129.8;
 		
-		System.out.println("Maior valor entre 27 e 5 é : "+Math.max(numero1, numero2));
-		System.out.println("A raiz quadrada de 27 é : "+Math.sqrt(numero1));
-		System.out.println("A potencia de 5 elevado a 27 é : "+Math.pow(numero2, numero1));
+		System.out.println("Maior valor entre 27 e 5 Ã© : "+Math.max(numero1, numero2));
+		System.out.println("A raiz quadrada de 27 Ã© : "+Math.sqrt(numero1));
+		System.out.println("A potencia de 5 elevado a 27 Ã© : "+Math.pow(numero2, numero1));
 		System.out.println("Arredondar o numero 129.8 = "+Math.round(numero3));
 		System.out.println("Arredondar para baixo:  "+Math.floor(numero3));
 	}
